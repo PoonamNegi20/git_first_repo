2c92e360ef2c7ade16ec85cd6b4173f9404fcae2
Poonam <poonamnegi1412@gmail.com>
2020-05-24 17:50:21 +0530

    added all variables and print out its result

--- GitDemoProject/ViewController.swift
+++ GitDemoProject/ViewController.swift
@@ -9,10 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var counter = 0
+    var increment:Int = 0{
+        didSet{
+            counter += 1
+            print("now i am changed\(counter)")
+        }
+        willSet{
+            print("i am going to change \(counter)")
+        }
+    }
+    
+    var result: Int{
+        get{
+            return counter
+        }
+        set{
+            counter = newValue
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       print("Welcome to my master branch")
+        print("counter :  \(counter)")
+        print("result :  \(result)")
+        print("counter after result called:  \(counter)")
+        increment = 1
+        print("counter :  \(counter)")
+        print("Increment :  \(increment)")
+        print("counter after increment:  \(counter)")
+           increment = 5
+        print("counter :  \(counter)")
+        print("Increment :  \(increment)")
+        print("counter after Increment:  \(counter)")
+        
+        result = 20
+          print("counter after result set 20:  \(counter)")
+        
     }
 
 
